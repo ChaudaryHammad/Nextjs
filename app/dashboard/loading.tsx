bc41c49488ffeeadd2d019831a80709e3e1b8167
ChaudaryHammad <ch.hammadbrw01@gmail.com>
2026-04-13 15:46:54 +0500

    Dasboard skeleton added

--- app/dashboard/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex h-96 items-center justify-center">
-      <p className="text-lg font-medium text-gray-500">Loading...</p>
-    </div>
-  );
-}
