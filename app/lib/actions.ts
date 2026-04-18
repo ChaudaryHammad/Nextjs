f75c642d14496c32f6c711c9dde44e8bb88fcd36
ChaudaryHammad <ch.hammadbrw01@gmail.com>
2026-04-18 15:11:50 +0500

    error boundary init + not-found

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,10 +24,17 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
    const date = new Date().toISOString().split('T')[0];
-await sql`
+try {
+  await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+} catch (error) {
+  console.error(error);
+  return {
+    message: 'An error occurred while creating the invoice.',
+  }
+}
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -46,17 +53,25 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
+try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+} catch (error) {
+  console.error(error);
+  return{
+    message: 'An error occurred while updating the invoice.',
+  }
+}
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function DeleteInvoiceById( id : string ) {
+  throw new Error('Not implemented yet');
     await sql`Delete from invoices WHERE id=${id}`;
     revalidatePath('/dashboard/invoices');
   
